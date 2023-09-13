3d284e98a36c3675e38092e40edef9e36731cdcd
Pietromuresu <muresupietro66@gmail.com>
2023-09-13 16:30:22 +0200

    Calculate price and apply a discount based on user's age

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,29 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double priceForKm = 0.21;
+		System.out.println("How many kilometers do you have to travel: ");
+		int kmToDo = sc.nextInt();
+
+		System.out.println("How old are you? ");
+		int userYears = sc.nextInt();
+		double price = kmToDo * priceForKm;
+		
+		if(userYears < 18) {
+			
+			price = price - (price * 0.2);
+		}else if(userYears > 65) {
+			
+			price = price - (price * 0.4);
+		}
+		
+		System.out.println(price);
+		sc.close();
+		
+	}
 }
